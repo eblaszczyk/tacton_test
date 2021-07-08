334537c2b4764218e82fb42e0bc54830349de1f2
eblaszczyk <eblaszczyk@pl.imshealth.com>
2021-07-08 15:55:41 +0200

    Initial commit

---
+++ src/main/java/com/eblaszczyk/SimpleCalculator.java
@@ -0,0 +1,86 @@
+package com.eblaszczyk;
+
+
+import java.util.Stack;
+
+public class SimpleCalculator {
+
+    private String[] ops;
+    private String[] nums;
+
+    public int calculate(String input) {
+
+        input = input.trim();
+
+        if (!checkIfInputIsCorrect(input)) {
+            throw new RuntimeException("Incorrect input");
+        }
+
+        String[] parts = input.split(" ");
+
+        if (parts.length == 1) {
+            return Integer.parseInt(parts[0]);
+        }
+
+        splitForNumsAndOps(parts);
+
+        return calculateExpression();
+    }
+
+    private int calculateExpression() {
+        Stack<Integer> numsStack = new Stack<>();
+        numsStack.push(Integer.parseInt(nums[0]));
+
+        for (int i = 0; i < ops.length; i++) {
+            int curr = Integer.parseInt(nums[i + 1]);
+
+            if (ops[i].equals("*")) {
+                curr = numsStack.pop() * curr;
+            }
+
+            if (ops[i].equals("/")) {
+                curr = numsStack.pop() / curr;
+            }
+
+            if (ops[i].equals("-")) {
+                curr = -curr;
+            }
+
+            numsStack.push(curr);
+        }
+
+        int result = 0;
+
+        while (numsStack.size() > 0) {
+            result += numsStack.pop();
+        }
+
+        return result;
+    }
+
+    private void splitForNumsAndOps(String[] parts) {
+        ops = new String[parts.length / 2];
+        nums = new String[parts.length / 2 + 1];
+
+        for (int i = 0; i < parts.length; i++) {
+            if (i % 2 == 1) {
+                ops[i / 2] = parts[i];
+            } else {
+                nums[i / 2] = parts[i];
+            }
+        }
+    }
+
+    private boolean checkIfInputIsCorrect(String input) {
+        return input != null && !input.isEmpty()
+                && input.matches("^-?\\d+( (\\+|-|\\*|\\/) -?\\d+)*");
+    }
+
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        System.out.println("Ex1 = " + calculator.calculate("2 + 3"));
+        System.out.println("Ex1 = " + calculator.calculate("3 * 2 + 1"));
+        System.out.println("Ex1 = " + calculator.calculate("3 * -2 + 6"));
+    }
+}
---
+++ src/test/java/com/eblaszczyk/SimpleCalculatorTest.java
@@ -0,0 +1,44 @@
+package com.eblaszczyk;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class SimpleCalculatorTest {
+
+    private SimpleCalculator calculator = new SimpleCalculator();
+
+    @Test
+    public void shouldReturnNumberWhenNoOperation() {
+        assertThat(calculator.calculate("-1")).isEqualTo(-1);
+        assertThat(calculator.calculate("1")).isEqualTo(1);
+        assertThat(calculator.calculate("-123")).isEqualTo(-123);
+    }
+
+    @Test
+    public void shouldCalculateExpresion(){
+        assertThat(calculator.calculate("-123 + 123 * 1 * 3")).isEqualTo(246);
+        assertThat(calculator.calculate("-123 + 123 / 1 + 2 * 3")).isEqualTo(6);
+        assertThat(calculator.calculate("-123 + 123 / 1 + 2 * -3")).isEqualTo(-6);
+    }
+
+    @Test
+    public void shouldThrowExceptionDueInvalidInput(){
+        assertThatExceptionOfType(RuntimeException.class) .isThrownBy(() ->
+                calculator.calculate("invalid input"));
+
+        assertThatExceptionOfType(RuntimeException.class) .isThrownBy(() ->
+                calculator.calculate("-5 * 6 a 4"));
+
+    }
+
+    @Test
+    public void shouldTrimAndNoThrowException(){
+        Assertions.assertThatNoException().isThrownBy(()->
+                calculator.calculate(" -123 + 123 / 1 + 2 * -3 "));
+    }
+
+
+}
